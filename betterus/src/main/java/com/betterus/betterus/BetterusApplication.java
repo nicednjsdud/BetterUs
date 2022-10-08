ac73de20983a322334cbd2cf3fec1f1f1b01c177
JeongWonYoung <nicednjsdud@naver.com>
2022-10-08 19:29:32 +0900

    Build project Betterus
    to start team project

---
+++ betterus/src/main/java/com/betterus/betterus/BetterusApplication.java
@@ -0,0 +1,13 @@
+package com.betterus.betterus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterusApplication.class, args);
+	}
+
+}
---
+++ betterus/src/test/java/com/betterus/betterus/BetterusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betterus.betterus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
