d336716cd448e743e85caacac8e059490f194363
JeongWonYoung <nicednjsdud@naver.com>
2022-10-19 01:29:36 +0900

    Add entity image

---
+++ betterus/src/main/java/com/betterus/entity/Image.java
@@ -0,0 +1,34 @@
+/**
+ * 작성자 : 정원영
+ * 작성 일자 : 2022 - 10 - 19
+ * 수정 일자 :
+ * 기능 : Image 테이블 엔티티
+ */
+package com.betterus.entity;
+
+import com.betterus.entity.common.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Image extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "imageId")
+    private Long id;
+
+    @Column(name = "imageFileName", length = 1000)
+    private String fileName;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "articleId")
+    private Article article;
+}
