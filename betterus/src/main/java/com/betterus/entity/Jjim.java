453cfe42730dcbc29878aa5ae0e12ca27a59c51a
JeongWonYoung <nicednjsdud@naver.com>
2022-10-19 01:29:45 +0900

    Add entity Jjim

---
+++ betterus/src/main/java/com/betterus/entity/Jjim.java
@@ -0,0 +1,35 @@
+/**
+ * 작성자 : 정원영
+ * 작성 일자 : 2022 - 10 - 19
+ * 수정 일자 :
+ * 기능 : Jjim 테이블 엔티티
+ */
+package com.betterus.entity;
+
+import com.betterus.entity.common.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Jjim extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "jjimId")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "memberId")
+    private Member member;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "articleId")
+    private Article article;
+}
