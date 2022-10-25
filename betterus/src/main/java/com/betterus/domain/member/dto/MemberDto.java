83d3da09815099b20cc6a2ef362f25232c269bcc
JeongWonYoung <nicednjsdud@naver.com>
2022-10-25 20:24:35 +0900

    Feat Member DTO, frm

---
+++ betterus/src/main/java/com/betterus/domain/member/dto/MemberDto.java
@@ -0,0 +1,35 @@
+/**
+ * 작성자 : 정원영
+ * 작성 일자 : 2022 - 10 - 22
+ * 수정 일자 :
+ * 기능 : Member DTO
+ */
+
+package com.betterus.domain.member.dto;
+
+import com.betterus.domain.member.domain.Member;
+import com.betterus.model.Grade;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class MemberDto {
+
+    private Long id;
+    private String nickName;
+    private String password;
+    private String email;
+    private Grade grade;
+    private String user_info;
+
+    public MemberDto(Member member){
+        id = member.getId();
+        nickName = member.getNickName();
+        password = member.getPassword();
+        email = member.getEmail();
+        grade = member.getGrade();
+        user_info = member.getUser_info();
+    }
+
+}
