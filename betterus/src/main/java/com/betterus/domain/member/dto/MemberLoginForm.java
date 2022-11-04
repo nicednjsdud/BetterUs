2225e3d6603e117d7b571bbbbaa772a4c9bc5d6f
JeongWonYoung <nicednjsdud@naver.com>
2022-11-04 15:18:09 +0900

    Add Member login form
    to use login

---
+++ betterus/src/main/java/com/betterus/domain/member/dto/MemberLoginForm.java
@@ -0,0 +1,17 @@
+/**
+ * 작성자 : 정원영
+ * 작성 일자 : 2022 - 11 - 03
+ * 수정 일자 :
+ * 기능 : 멤버 로그인 폼용
+ */
+
+package com.betterus.domain.member.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemberLoginForm {
+
+    private String email;
+    private String password;
+}
