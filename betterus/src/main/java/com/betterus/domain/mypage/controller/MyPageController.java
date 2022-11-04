e8b136a4f5f2a13374f1175be9faeb1e7c2f490f
JeongWonYoung <nicednjsdud@naver.com>
2022-11-04 15:22:06 +0900

    Add MyPageController

---
+++ betterus/src/main/java/com/betterus/domain/mypage/controller/MyPageController.java
@@ -0,0 +1,16 @@
+/**
+ * 작성자 : 정원영
+ * 작성 일자 : 2022 - 11 - 04
+ * 수정 일자 :
+ * 기능 : 마이페이지 컨트롤러
+ */
+
+package com.betterus.domain.mypage.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@RequiredArgsConstructor
+@Controller
+public class MyPageController {
+}
