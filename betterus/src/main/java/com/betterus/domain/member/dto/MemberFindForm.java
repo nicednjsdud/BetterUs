b4414de4915cb8096cd0fb3e02fc527655956827
JeongWonYoung <nicednjsdud@naver.com>
2022-11-04 15:17:06 +0900

    Add MemberFindForm
    form to send email

---
+++ betterus/src/main/java/com/betterus/domain/member/dto/MemberFindForm.java
@@ -0,0 +1,16 @@
+/**
+ * 작성자 : 정원영
+ * 작성 일자 : 2022 - 11 - 03
+ * 수정 일자 :
+ * 기능 : 멤버 비밀번호 찾기 폼용
+ */
+
+package com.betterus.domain.member.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemberFindForm {
+
+    private String email;
+}
