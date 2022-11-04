b767c5734ec2556f03c5791fb2d5135b9b667e37
JeongWonYoung <nicednjsdud@naver.com>
2022-11-04 15:19:17 +0900

    Feat MemberRepository
    feat findByNickName, findByEmailAndPassword, findByEmail

--- betterus/src/main/java/com/betterus/domain/member/repository/MemberRepository.java
+++ betterus/src/main/java/com/betterus/domain/member/repository/MemberRepository.java
@@ -1,9 +1,26 @@
+/**
+ * 작성자 : 정원영
+ * 작성 일자 : 2022 - 11 - 03
+ * 수정 일자 :
+ * 기능 : 멤버 repository
+ */
+
 package com.betterus.domain.member.repository;
 
 import com.betterus.domain.member.domain.Member;
 import org.springframework.dao.DataAccessException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface MemberRepository {
+import java.util.List;
 
-    void save(Member member) throws DataAccessException;
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Member findByEmailAndPassword(String email, String password);
+
+    Member findByEmail(String email);
+
+    Member findByNickName(String nickName);
+
 }
