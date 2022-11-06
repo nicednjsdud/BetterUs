bd0f7029d773d322ecdefeb0d9035b3682852a6b
JeongWonYoung <nicednjsdud@naver.com>
2022-11-07 00:58:57 +0900

    Add Column image
    add fullPath

--- betterus/src/main/java/com/betterus/domain/article/domain/Image.java
+++ betterus/src/main/java/com/betterus/domain/article/domain/Image.java
@@ -1,7 +1,7 @@
 /**
  * 작성자 : 정원영
  * 작성 일자 : 2022 - 10 - 19
- * 수정 일자 :
+ * 수정 일자 : 2022 - 11 - 06
  * 기능 : Image 테이블 엔티티
  */
 package com.betterus.domain.article.domain;
@@ -27,6 +27,9 @@ public class Image extends BaseTimeEntity {
 
     @Column(name = "imageFileName", length = 1000)
     private String fileName;
+
+    @Column(name = "fullPath", length = 1000)
+    private String fullPath;
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "articleId")
