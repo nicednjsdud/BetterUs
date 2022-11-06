f08137284afdfedf4d748a9a3c8eeec8dee01535
JeongWonYoung <nicednjsdud@naver.com>
2022-11-07 01:00:09 +0900

    Add ArticleDTO

---
+++ betterus/src/main/java/com/betterus/domain/article/dto/ArticleDto.java
@@ -0,0 +1,29 @@
+/**
+ * 작성자 : 정원영
+ * 작성 일자 : 2022 - 11 - 06
+ * 수정 일자 :
+ * 기능 : Article dto 화면 뿌리기용
+ */
+
+package com.betterus.domain.article.dto;
+
+import com.betterus.domain.articleseries.domain.ArticleSeries;
+import com.betterus.domain.member.domain.Member;
+import com.betterus.model.ArticleStatus;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+
+@NoArgsConstructor
+@Getter
+public class ArticleDto {
+
+    private Long id;
+    private String title;
+    private String subTitle;
+    private String contents;
+    private ArticleStatus status;
+}
