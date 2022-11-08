367b7e410383b251829e925828948be64867787d
JeongWonYoung <nicednjsdud@naver.com>
2022-11-09 01:26:44 +0900

    Add @Setter at Article Entity

--- betterus/src/main/java/com/betterus/domain/article/domain/Article.java
+++ betterus/src/main/java/com/betterus/domain/article/domain/Article.java
@@ -1,7 +1,7 @@
 /**
  * 작성자 : 정원영
  * 작성 일자 : 2022 - 10 - 19
- * 수정 일자 :
+ * 수정 일자 : 2022 - 11 - 09
  * 기능 : Article 테이블 엔티티
  */
 package com.betterus.domain.article.domain;
@@ -10,10 +10,7 @@ import com.betterus.domain.member.domain.Member;
 import com.betterus.domain.articleseries.domain.ArticleSeries;
 import com.betterus.model.ArticleStatus;
 import com.betterus.model.BaseTimeEntity;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -21,6 +18,7 @@ import static javax.persistence.FetchType.LAZY;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article extends BaseTimeEntity {
 
@@ -56,4 +54,5 @@ public class Article extends BaseTimeEntity {
         this.status = status;
         this.member = member;
     }
+
 }
