43ac415592d1faf1d69f46b8bdb29a4ed96d7f35
JeongWonYoung <nicednjsdud@naver.com>
2022-11-16 01:33:49 +0900

    Test changeInfo

--- betterus/src/test/java/com/betterus/domain/member/service/MemberServiceImplTest.java
+++ betterus/src/test/java/com/betterus/domain/member/service/MemberServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.betterus.domain.member.service;
 
 import com.betterus.domain.member.domain.Member;
+import com.betterus.domain.member.dto.MemberEditForm;
 import com.betterus.domain.member.repository.MemberRepository;
 import com.betterus.model.Grade;
 import org.assertj.core.api.Assertions;
@@ -12,6 +13,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -68,4 +71,39 @@ class MemberServiceImplTest {
         assertThat(findMember.getPassword()).isEqualTo(saveMember.getPassword());
         assertThat(findMember.getUser_info()).isEqualTo(null);
     }
+
+    @Test
+    @DisplayName("멤버 아이디로 조회하기")
+    public void findMemberById(){
+        Member member = new Member("MemberA", "123123", "nicednjsdud@gmail.com", Grade.ADMIN);
+        Member saveMember = memberRepository.save(member);
+
+        em.flush();
+        em.clear();
+
+        Member findMember = memberService.findMemberById(saveMember.getId());
+
+        assertThat(findMember.getNickName()).isEqualTo(saveMember.getNickName());
+        assertThat(findMember.getPassword()).isEqualTo(saveMember.getPassword());
+        assertThat(findMember.getUser_info()).isEqualTo(null);
+    }
+
+    @Test
+    @DisplayName("회원 정보 변경 - 닉네임변경")
+    public void changeInfo(){
+        Member member = new Member("MemberA", "123123", "nicednjsdud@gmail.com", Grade.ADMIN);
+        Member saveMember = memberRepository.save(member);
+        Long memberId = saveMember.getId();
+        em.flush();
+        em.clear();
+
+        MemberEditForm changeNickNameForm = new MemberEditForm(null,"MemberB",null,null,"닉네임변경");
+        int result = memberService.changeMemberInfo(memberId, changeNickNameForm);
+
+        Member findMember = em.find(Member.class, memberId);
+
+        assertThat(findMember.getNickName()).isEqualTo("MemberB");
+        assertThat(result).isEqualTo(1);
+        }
+
 }
