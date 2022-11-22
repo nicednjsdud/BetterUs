c3af47b2618cee4bc29a0705ea0ccd652646fe19
JeongWonYoung <nicednjsdud@naver.com>
2022-11-22 22:37:37 +0900

    Feat GudokService delete gudok method

--- betterus/src/main/java/com/betterus/domain/gudok/service/GudokService.java
+++ betterus/src/main/java/com/betterus/domain/gudok/service/GudokService.java
@@ -3,6 +3,7 @@ package com.betterus.domain.gudok.service;
 import com.betterus.domain.member.domain.Member;
 
 public interface GudokService {
-    String addGudok(Member member, Long articleId);
+    int addGudok(Member member, Long articleId);
 
+    int deleteGudok(Member member, Long authorId);
 }
