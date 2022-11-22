eabcaf0e7dcc265e5a4c4af7709f43b7134c2ff1
JeongWonYoung <nicednjsdud@naver.com>
2022-11-22 22:37:59 +0900

    Feat Membercontroller Authorlist page

--- betterus/src/main/java/com/betterus/domain/member/controller/MemberController.java
+++ betterus/src/main/java/com/betterus/domain/member/controller/MemberController.java
@@ -1,7 +1,7 @@
 /**
  * 작성자 : 정원영
  * 작성 일자 : 2022 - 11 - 03
- * 수정 일자 : 2022 = 11 - 16
+ * 수정 일자 : 2022 = 11 - 18
  * 기능 : MemberController
  */
 
@@ -9,19 +9,28 @@
 package com.betterus.domain.member.controller;
 
 
+import com.betterus.domain.article.dto.ArticleDto;
 import com.betterus.domain.email.service.EmailService;
 import com.betterus.domain.member.domain.Member;
+import com.betterus.domain.member.dto.MemberDto;
 import com.betterus.domain.member.dto.MemberEditForm;
 import com.betterus.domain.member.dto.MemberJoinForm;
 import com.betterus.domain.member.service.MemberService;
 import com.betterus.model.Grade;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -30,6 +39,52 @@ public class MemberController {
     private final MemberService memberService;
 
     private final EmailService emailService;
+
+    /**
+     *  작가 리스트 (패이징)
+     *  구독자가 많은 수 대로
+     */
+    @GetMapping("/list/author")
+    public String authorList(@PageableDefault(size = 10)Pageable pageable,Model model, HttpServletRequest request) {
+        String msg = "";
+        HttpSession session = request.getSession();
+        Object member = session.getAttribute("member");
+        if (member != null) {
+            Page<MemberDto> authors = memberService.findAuthorByGrade(pageable);
+            int nowPage =  authors.getPageable().getPageNumber();
+            int startPage = Math.max(nowPage - 4,1);
+            int endPage = Math.min(nowPage + 5, authors.getTotalPages());
+            model.addAttribute("list",authors);
+            model.addAttribute("nowPage",nowPage);
+            model.addAttribute("startPage",startPage);
+            model.addAttribute("endPage",endPage);
+        }
+        else{
+            msg = "회원만 보기가 가능합니다.";
+            model.addAttribute("msg",msg);
+            return "redirect:/";
+        }
+        return "화면 구현 안됨";
+    }
+
+    /**
+     *  작가 상세 페이지 (패이징) 진행중
+     */
+    @GetMapping("/list/authorPage/{authorId}/default")
+    public String authorInfo(@PathVariable("authorId")Long authorId,@PageableDefault(size = 10,sort = "createDate") Pageable pageable, Model model, HttpServletRequest request) {
+        String msg = "";
+        HttpSession session = request.getSession();
+        Object member = session.getAttribute("member");
+        if (member != null) {
+//            memberService.findMemberById(authorId)
+        }
+        else{
+            msg = "회원만 보기가 가능합니다.";
+            model.addAttribute("msg",msg);
+            return "redirect:/";
+        }
+        return "화면 구현 안됨";
+    }
 
     /**
      * 회원 가입 폼
