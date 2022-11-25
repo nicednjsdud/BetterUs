778cc608093d4a43260a94e6c4db35c2a7f48a13
JeongWonYoung <nicednjsdud@naver.com>
2022-11-25 17:28:29 +0900

    Feat MyPage entity
    add join mypage form

--- betterus/src/main/java/com/betterus/domain/mypage/domain/MyPage.java
+++ betterus/src/main/java/com/betterus/domain/mypage/domain/MyPage.java
@@ -1,8 +1,16 @@
+/**
+ * 작성자 : 정원영
+ * 작성 일자 : 2022 - 11 - 04
+ * 수정 일자 : 2022 = 11 = 22
+ * 기능 : 마이페이지 엔티티
+ */
+
 package com.betterus.domain.mypage.domain;
 
 import com.betterus.domain.article.domain.Article;
 import com.betterus.domain.member.domain.Member;
-import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
@@ -14,7 +22,8 @@ import static javax.persistence.FetchType.LAZY;
 
 @Entity
 @NoArgsConstructor
-@Getter
+@AllArgsConstructor
+@Data
 public class MyPage {
 
     @Id
@@ -29,5 +38,16 @@ public class MyPage {
     @OneToMany(mappedBy = "myPage")
     private List<Article> articleList = new ArrayList<>();
 
+    /**
+     * 회원 가입 초기 마이페이지 생성
+     */
+    public MyPage(Member member) {
+        this.member = member;
+    }
+
+    public MyPage(Member member, List<Article> articleList) {
+        this.member = member;
+        this.articleList = articleList;
+    }
 
 }
