ab8233d262ae5f0c93f7db1eb5bd8ec0487d7ccc
JeongWonYoung <nicednjsdud@naver.com>
2022-11-25 17:25:47 +0900

    Feat gudokRepository
    add findGudokTrue

--- betterus/src/main/java/com/betterus/domain/gudok/repository/GudokRepository.java
+++ betterus/src/main/java/com/betterus/domain/gudok/repository/GudokRepository.java
@@ -2,7 +2,14 @@ package com.betterus.domain.gudok.repository;
 
 import com.betterus.domain.gudok.domain.Gudok;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface GudokRepository extends JpaRepository<Gudok, Long> {
     int findByMemberId(Long id);
+
+    @Query("select g from Gudok g left join fetch g.member m where g.authorId = :authorId and m.id = :memberId" )
+    Gudok findGudokTure(@Param("authorId") Long authorId,@Param("memberId") Long memberId);
 }
