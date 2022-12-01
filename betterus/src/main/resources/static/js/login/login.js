e38ec9e72b1692a0c64f472b2af16e360523d235
JeongWonYoung <nicednjsdud@naver.com>
2022-12-02 05:26:40 +0900

    Change html 경로

--- betterus/src/main/resources/static/js/login/login.js
+++ betterus/src/main/resources/static/js/login/login.js
@@ -3,14 +3,19 @@ const loginButton = document.querySelector(".section button");
 
 let emailRegExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
 
+
 function loginLink(event){
+
+
   event.preventDefault();
   let isVaild=emailRegExp.test(loginInput.value);
   if(!isVaild){
     alert("유효하지 않은 이메일입니다.");
     return;
-  } 
-  location.href = "loginSuccess";
+  }
+
+ let form = document.getElementById("login");
+ form.submit();
 }
-  
+
 loginButton.addEventListener("click", loginLink);
