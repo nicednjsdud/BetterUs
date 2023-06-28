8ce3394448432733f409f4fa7490d7442847f0d9
DESKTOP-1VCQD60\nongg <kwak.fe@gmail.com>
2023-06-28 20:59:37 +0900

    main page js correction

--- betterus/src/main/resources/static/js/main/slide.js
+++ betterus/src/main/resources/static/js/main/slide.js
@@ -1,13 +1,27 @@
-const rightSlideButton = document.querySelector(".slide__btn-right");
-const leftSlideButton = document.querySelector(".slide__btn-left");
+const editorPicRightSlideButton = document.querySelector(
+  ".slide__btn-right.editor__pic-btn"
+);
+const editorPicLeftSlideButton = document.querySelector(
+  ".slide__btn-left.editor__pic-btn"
+);
 const slides = document.querySelector(".slides");
 const slideIndex = document.querySelector(".wrap_slide__index");
+const recmmndedArticlesSlides = document.querySelector(
+  ".recmmnded-articles-slides"
+);
+const articlesRightSlideButton = document.querySelector(
+  ".slide__btn-right.recmmnded-articles-btn"
+);
+const articlesLeftSlideButton = document.querySelector(
+  ".slide__btn-left.recmmnded-articles-btn"
+);
 
 let slideNumber = 0;
 const MAX = slides.childElementCount - 1;
 const MIN = 0;
+let movedLength = 0;
 
-function setTransform() {
+function editorPicSetTransform() {
   slides.setAttribute(
     "style",
     `transform: translateX(${-960 * slideNumber}px)`
@@ -17,28 +31,29 @@ function setTransform() {
   slideIndex.children[slideNumber].classList.add("emphasis");
 
   if (slideNumber == MAX) {
-    rightSlideButton.classList.add("slide__btn-disappear");
-    leftSlideButton.classList.remove("slide__btn-disappear");
+    editorPicRightSlideButton.classList.add("slide__btn-disappear");
+    editorPicLeftSlideButton.classList.remove("slide__btn-disappear");
   } else if (slideNumber == MIN) {
-    leftSlideButton.classList.add("slide__btn-disappear");
-    rightSlideButton.classList.remove("slide__btn-disappear");
+    editorPicLeftSlideButton.classList.add("slide__btn-disappear");
+    editorPicRightSlideButton.classList.remove("slide__btn-disappear");
   } else {
-    leftSlideButton.classList.remove("slide__btn-disappear");
-    rightSlideButton.classList.remove("slide__btn-disappear");
+    editorPicLeftSlideButton.classList.remove("slide__btn-disappear");
+    editorPicRightSlideButton.classList.remove("slide__btn-disappear");
   }
 }
 
-function rightSlide() {
+function editorPicRightSlide() {
   if (slideNumber < MAX) {
     slideNumber += 1;
-    setTransform();
+    editorPicSetTransform();
   }
+  console.log("hi");
 }
 
-function leftSlide() {
+function editorPicLeftSlide() {
   if (slideNumber > MIN) {
     slideNumber -= 1;
-    setTransform();
+    editorPicSetTransform();
   }
 }
 
@@ -48,12 +63,47 @@ function indexEmphasis() {
   this.classList.add("emphasis");
 
   slideNumber = parseInt(this.getAttribute("value"));
-  setTransform();
+  editorPicSetTransform();
+}
+
+function articlesSetTransform() {
+  recmmndedArticlesSlides.setAttribute(
+    "style",
+    `transform: translateX(${movedLength}px)`
+  );
+
+  if (movedLength == -4800) {
+    articlesRightSlideButton.classList.add("slide__btn-disappear");
+    articlesLeftSlideButton.classList.remove("slide__btn-disappear");
+  } else if (movedLength == 0) {
+    articlesLeftSlideButton.classList.add("slide__btn-disappear");
+    articlesRightSlideButton.classList.remove("slide__btn-disappear");
+  } else {
+    articlesLeftSlideButton.classList.remove("slide__btn-disappear");
+    articlesRightSlideButton.classList.remove("slide__btn-disappear");
+  }
+}
+
+function articlesRightSlide() {
+  if (movedLength > -4800) {
+    movedLength -= 960;
+    articlesSetTransform();
+  }
+}
+
+function articlesLeftSlide() {
+  if (movedLength < 0) {
+    movedLength += 960;
+    articlesSetTransform();
+  }
 }
 
 for (let i = 0; i <= MAX; i++) {
   slideIndex.children[i].addEventListener("click", indexEmphasis);
 }
 
-rightSlideButton.addEventListener("click", rightSlide);
-leftSlideButton.addEventListener("click", leftSlide);
+editorPicRightSlideButton.addEventListener("click", editorPicRightSlide);
+editorPicLeftSlideButton.addEventListener("click", editorPicLeftSlide);
+
+articlesRightSlideButton.addEventListener("click", articlesRightSlide);
+articlesLeftSlideButton.addEventListener("click", articlesLeftSlide);
